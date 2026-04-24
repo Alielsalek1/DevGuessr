d37a87981225e457c8bc99e5282cd771ec71c33c
Ali Abdelrahman <aliabdelrahman2005@gmail.com>
2026-04-24 15:05:16 +0300

    som seo

--- Frontend/src/app/core/services/seo.service.ts
+++ Frontend/src/app/core/services/seo.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Title, Meta } from '@angular/platform-browser';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { filter, map, mergeMap } from 'rxjs/operators';
+import { DOCUMENT } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,20 @@ export class SeoService {
     private titleService: Title,
     private metaService: Meta,
     private router: Router,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    @Inject(DOCUMENT) private dom: Document
   ) {}
+
+  private updateCanonicalUrl(url: string) {
+    const head = this.dom.getElementsByTagName('head')[0];
+    let element: HTMLLinkElement | null = this.dom.querySelector(`link[rel='canonical']`) || null;
+    if (element === null) {
+      element = this.dom.createElement('link') as HTMLLinkElement;
+      element.setAttribute('rel', 'canonical');
+      head.appendChild(element);
+    }
+    element.setAttribute('href', url);
+  }
 
   init() {
     this.router.events.pipe(
@@ -30,14 +43,18 @@ export class SeoService {
       if (data['title']) {
         this.titleService.setTitle(data['title']);
         this.metaService.updateTag({ property: 'og:title', content: data['title'] });
+        this.metaService.updateTag({ name: 'twitter:title', content: data['title'] });
       }
       
       if (data['description']) {
         this.metaService.updateTag({ name: 'description', content: data['description'] });
         this.metaService.updateTag({ property: 'og:description', content: data['description'] });
+        this.metaService.updateTag({ name: 'twitter:description', content: data['description'] });
       }
 
-      this.metaService.updateTag({ property: 'og:url', content: window.location.href });
+      const url = `https://devguessr.site${this.router.url.split('?')[0].split('#')[0]}`;
+      this.metaService.updateTag({ property: 'og:url', content: url });
+      this.updateCanonicalUrl(url);
     });
   }
 }
